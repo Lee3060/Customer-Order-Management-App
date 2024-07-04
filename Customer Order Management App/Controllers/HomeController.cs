0db3635f3876d3ded3a7a05f89bfbbe7017d6782
Lee3060 <rahulprajapati3060@gmail.com>
2024-07-05 01:32:26 +0530

    Customer Model with Annotations Added

--- Customer Order Management App/Controllers/HomeController.cs
+++ Customer Order Management App/Controllers/HomeController.cs
@@ -6,12 +6,12 @@ namespace Customer_Order_Management_App.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        //private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
 
         public IActionResult Index()
         {
---
+++ Customer Order Management App/Models/Customer.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Customer_Order_Management_App.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int CustmerId { get; set; }
+
+        [Column(TypeName = "varchar(100)")]
+        [Required(ErrorMessage = "CustomerName is must")]
+        public string? CustomerName { get; set; }
+
+        [Required(ErrorMessage = "CustomerAge is must")]
+        [Column(TypeName = "varchar(20)")]
+        public int CustomerAge { get; set; }
+
+        [Required(ErrorMessage = "ProductName is must")]
+        [Column(TypeName = "varchar(100)")]
+        public string? ProductName { get; set; }
+
+        [Required(ErrorMessage = "OrderDate is must")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime OrderDate { get; set; }
+
+        [Required(ErrorMessage = "OrderId is must")]
+        public int OrderId { get; set; }
+    }
+}
