25ef8045d0100a9aeaca9b7df45c91a6695a2706
Lee3060 <rahulprajapati3060@gmail.com>
2024-07-21 21:55:14 +0530

    Create, Details and Edit method added in controller and thier views created.

--- Customer Order Management App/Controllers/HomeController.cs
+++ Customer Order Management App/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 using Customer_Order_Management_App.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using System.Net;
 
 namespace Customer_Order_Management_App.Controllers
 {
@@ -20,11 +23,73 @@ namespace Customer_Order_Management_App.Controllers
             this.customerDb = customerDb;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            var custData = customerDb.Customers.ToList();
+            var custData = await customerDb.Customers.ToListAsync();
             return View(custData);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Customer cust)
+        {
+            if (ModelState.IsValid)
+            {
+                await customerDb.Customers.AddAsync(cust);
+                await customerDb.SaveChangesAsync();
+                return RedirectToAction("Index", "Home");
+            }
+            return View(cust);
+        }
+
+        public async Task<IActionResult> Details(int? id)  //Due to ? if id can be null
+        {
+            if(id == null || customerDb.Customers == null)
+            {
+                return NotFound();
+            }
+            var custData = await customerDb.Customers.FirstOrDefaultAsync(x => x.CustmerId == id);
+            if(customerDb == null)
+            {
+                return NotFound();
+            }
+            return View(custData);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || customerDb.Customers == null)
+            {
+                return NotFound();
+            }
+            var custData = await customerDb.Customers.FindAsync(id);
+            if (customerDb == null)
+            {
+                return NotFound();
+            }
+            return View(custData);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Edit(int? id, Customer cust)
+        {
+            if(id != cust.CustmerId)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                customerDb.Customers.Update(cust);
+                await customerDb.SaveChangesAsync();
+                return RedirectToAction("Index", "Home");   
+            }
+            return View(cust);
+        }
+
 
         public IActionResult Privacy()
         {
